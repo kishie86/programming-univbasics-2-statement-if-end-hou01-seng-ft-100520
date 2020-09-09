099481a3b5820fbf99f8909b36baea21aa0bd8f2
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-09 23:08:20 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,8 +2,6 @@ current_time = Time.now
 current_time = current_time.to_i 
 if current_time % 2 == 0 
   puts "Even!"
-end 
-
-current_time = Time.now 
-current_time = current_time.to_i
-if current_time % 
+else
+  puts "Odd!"
+end
